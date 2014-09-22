9ddbcf9d1f27b40ff10f029ad38cf3949d56fc8d
Coral <jonas@jonasbengtson.se>
2014-09-22 10:25:14 +0200

    HAHA KORV

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 ///////////////////////
 // OK THIS IS SOMEWHAT MADE TO SEND STUFF OVER SOCKET FROM HURFSTONE?
 // WOW
+// THIS IS JUST A SHITTY BROADCAST
 ///////////////////////
-///
+
 var _ = require('lodash');
-var
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
+
+server.listen(7162);
+
+io.on('connection', function (socket) {
+
+	socket.on('turn', function (data) {
+		socket.broadcast.emit('turn', data);
+	});
+
+});
