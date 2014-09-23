13b3c3d6cd6e2298c863fd3aa38633e38d17d7b6
Coral <jonas@jonasbengtson.se>
2014-09-23 12:41:17 +0200

    HAHA JESUS CHRIST THIS CODE SUCKS

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 ///////////////////////
-// OK THIS IS SOMEWHAT MADE TO SEND STUFF OVER SOCKET FROM HURFSTONE?
-// WOW
-// THIS IS JUST A SHITTY BROADCAST
+/// OK THIS IS SOMEWHAT MADE TO SEND STUFF OVER SOCKET FROM HURFSTONE?
+/// WOW
+/// THIS IS JUST A SHITTY BROADCAST
 ///////////////////////
 
 var _ = require('lodash');
@@ -9,25 +9,39 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 
-var Triggers = require('./triggers.js').Triggers;
+var Trigger = require('./triggers.js').Triggers;
 var cfg = require('./config.json');
+
 var active_triggers;
+var state = {computer: "none"};
 
 server.listen(7162);
 
-Triggers.loadTriggers();
+Trigger.loadTriggers();
 
 // BOOOORING BOOORING BOOOOOOORING
 // WHATYA DOING GAOEHGOHFFOUHFAO
 
 io.on('connection', function (socket) {
 
+	socket.emit('status', {latch: Trigger.getLatch(), state: state});
+
 	socket.on('turn', function (data) {
 		if(data.turn ==  true)
 		{
+			console.log("Fire");
+			state = data;
 			Trigger.fire(data.computer);
+
 		}
 		socket.broadcast.emit('turn', data);
+	});
+
+	socket.on('latch', function (data)
+	{
+		Trigger.setLatch(data.enable);
+		socket.emit('status', {latch: Trigger.getLatch(), state: state});
+		socket.broadcast.emit('status', {latch: Trigger.getLatch(), state: state});
 	});
 
 });
--- triggers.js
+++ triggers.js
@@ -5,14 +5,18 @@
 (function Triggers(ns) {
 
 	var requestify = require('requestify');
+	var _ = require('lodash');
 	var cfg = require('./config.json');
 	var listener = require('socket.io-client')(cfg.router, {reconnection: 'false'});
 
 	var active_triggers;
+	var latch = true;
 
 	listener.on('connect', function(){
 		console.log("Connected");
-		listener.emit('register', {production: cfg.production});
+		//listener.emit('register', {production: cfg.production});
+		//
+		listener.emit('start', {production: cfg.production});
 	});
 
 	Triggers.loadTriggers = function() {
@@ -20,15 +24,26 @@
 			var parsed = response.getBody();
 			active_triggers = parsed.triggers;
 			console.log("Updated Triggers");
-			console.log(active_triggers);
 		});
 	}
 
-	Triggers.fire = function (ident) {
-		var trig = _.where(active_triggers, {trigger: cfg.identifiers[ident]})
-		listener.emit('commandQueue', {
-			command: JSON.parse(trig[0].command)
-		});
+	Triggers.fire = function (identifier) {
+		var trig = _.where(active_triggers, {trigger: cfg.identifiers[identifier].toString()});
+		if(latch) {
+			listener.emit('commandQueue', {
+				command: JSON.parse(trig[0].command)
+			});
+			console.log(JSON.parse(trig[0].command));
+		}
+	}
+
+	Triggers.setLatch = function(enable) {
+		latch = enable;
+		return true;
+	}
+
+	Triggers.getLatch = function() {
+		return latch;
 	}
 
 
